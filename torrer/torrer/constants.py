5e500ef6ebbf74a86cff28a9e9b7005b0ed8b8c4
Rowan Torbitzky-Lane <skrublaub@cognitiveweeds.net>
2022-07-23 21:32:51 -0500

    init constants

---
+++ torrer/torrer/constants.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+DEFAULT_URL: Final[str] = "localhost:8080"
+DEFAULT_USERNAME: Final[str] = "admin"
+
+TORRENTING_WEBSITE: Final[str] = "https://thepiratebay0.org"  # this mirror doesn't require javascript
