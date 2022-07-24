e32b8b443e508fcfff8a39b433482ae89df205bd
Rowan Torbitzky-Lane <skrublaub@cognitiveweeds.net>
2022-07-23 21:25:18 -0500

    add setup.py

---
+++ torrer/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+packages_list: list[str] = [
+    "selenium",  # also installs requests
+    "typer",
+    "qbittorrent-api",
+    "requests-html",
+    "beautifulsoup4"
+]
+
+setup(
+    name="torrer",
+    description="Hooks into qbittorrent to get torrents that I want",
+    author="skrublaub",
+    url="https://github.com/Skrublaub/useless-torrenter",
+    version="0.0.1",
+    entry_points={"console_scripts": ["torrer=torrer.__main__:main"]},
+)
