0b1807fcbcdde2d6c63d3541507d49d0db04f685
Rowan Torbitzky-Lane <skrublaub@cognitiveweeds.net>
2022-07-31 23:05:14 -0500

    add selenium command and rename torrent command

--- torrer/torrer/cli/base.py
+++ torrer/torrer/cli/base.py
@@ -1,39 +1,50 @@
-import qbittorrentapi as qb
+import logging
+
 from typer import Typer, Option, Argument
 
-from torrer.constants import DEFAULT_URL, DEFAULT_USERNAME, TORRENTING_WEBSITE
-from torrer.pirates_bay_requests.interface import search, parse_search_result
+from torrer.constants import DEFAULT_URL, REQUESTS_TORRENTING_WEBSITE, DEFAULT_SELENIUM_DOWNLOAD_PATH, SELENIUM_TORRENTING_WEBSITE
+from torrer.torrent.requests_interface import search, parse_search_result
+from torrer.torrent.qb_utils import add_to_qb
+from torrer.torrent.selenium_torrent import magnet_link_process
 
 cmd: Typer = Typer()
 
-
-@cmd.command(help="command to actually torrent something")
-def torrent(
-    host: str = Option(
-        DEFAULT_URL, "-l", "--localhost", help="hostname of qbittorent web API"
-    ),
-    username: str = Option(
-        DEFAULT_USERNAME, "-u", "--username", help="username to login"
-    ),
-    password: str = Option(..., "-p", "--password", help="password to login with"),
-) -> None:
-    qb_client = qb.Client(host=host, username=username, password=password)
-
-    try:
-        qb_client.auth_log_in()
-    except qb.LoginFailed as e:
-        print(e)
-
-    for indiv_torrent in qb_client.torrents_info():
-        print(indiv_torrent.name)
+logger = logging.getLogger(__name__)
 
 
-@cmd.command(help="print html for piratesbay")
-def info(
+@cmd.command(help="parse piratesbay with requests and bs4")
+def request(
     website: str = Option(
-        TORRENTING_WEBSITE, "-w", "--website", help="piratesbay link"
+        REQUESTS_TORRENTING_WEBSITE, "-w", "--website", help="very legal torrenting website link"
     ),
-    query: str = Argument(..., help="query to send to pirates bay"),
+    query: str = Argument(..., help="query to send to very legal torrenting website"),
+    qb_url: str = Option(
+        DEFAULT_URL, "-l", "--localhost", help="url of qbittorent web API"
+    ),
 ) -> None:
+    logger.info("Requests used to parse")
     top_result: str = search(website, query)
-    magnet_link = parse_search_result(top_result)
+    magnet_link: str = parse_search_result(top_result)
+    add_to_qb(magnet_link, qb_url)
+    return
+
+
+@cmd.command(help="""
+interact with piratesbay using selenium.
+If the selenium driver is not on path, it will download it.""")
+def selenium(
+    website: str = Option(
+        SELENIUM_TORRENTING_WEBSITE, "-w", "--website", help="very legal torrenting website link"
+    ),
+    query: str = Argument(..., help="query to send to very legal torrenting website"),
+    qb_url: str = Option(
+        DEFAULT_URL, "-l", "--localhost", help="url of qbittorent web API"
+    ),
+    selenium_path: str = Option(
+        DEFAULT_SELENIUM_DOWNLOAD_PATH, "-d", "--download-path", help="Path to download the geckodriver to"
+    )
+) -> None:
+    logger.info("Selenium used")
+    magnet_link: str = magnet_link_process(website, query, selenium_path)
+    add_to_qb(magnet_link, qb_url)
+    return
